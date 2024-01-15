a07b811e94595ccd251b67e321417e568d0a034d
inderjeetsingh1710 <inderjeet.singh.1@netsolutions.com>
2024-01-15 10:21:42 +0530

    Test comming

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import { Link } from "react-router-dom";
-//import books from "../datastore.json"
+//Home page
 function MyBanner(){
   return (
     <div className="banner">        
