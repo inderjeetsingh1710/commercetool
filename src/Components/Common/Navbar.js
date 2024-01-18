c7fd7a2f374e608f595a54ebf6831787c1445d82
Gagan Sood <gagan.sood@netsolutions.com>
2024-01-18 12:27:31 +0530

    changes updating

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Login from '../Login';
 
 
@@ -19,14 +19,14 @@ function Navbar(props) {
     <div className="header">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <a className="navbar-brand" href="#"><img src="/logo.png" alt="" width="175"/></a>
+            <NavLink to="/"><img src="/logo.png" alt="" width="175"/></NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item"><Link to="home">Home</Link></li>
-                <li className="nav-item"><Link to="#">Shop</Link></li>
-                <li className="nav-item"><Link to="#">Offers</Link></li>               
-                <li className="nav-item"><Link to="contact">Contact us</Link></li>
+              <NavLink to="/"><li className="nav-item">Home</li></NavLink>
+              <NavLink to="#"><li className="nav-item">Shop</li></NavLink>
+              <NavLink to="#"><li className="nav-item">Offers</li></NavLink>
+              <NavLink to="contact"><li className="nav-item">Contact us</li></NavLink>
                 {isLogged ? (<li className="nav-item">
                       <Link to="login"><span>Log in</span></Link>
                 </li>):(<li className="nav-item">
