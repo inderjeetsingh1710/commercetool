d99bda829ae0563d37bdfcd2f764ef89b0ebcd96
vineetnetsol <108414384+vineetnetsol@users.noreply.github.com>
2024-01-21 21:12:59 +0530

    PDP component - fetched product data

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
-function ProductDetail(props) {
-    
+function ProductDetail() {
+const [product, setProduct] = useState({});
+const {id} = useParams();
+useEffect(()=>{
+  axios.get(process.env.REACT_APP_API_HOST_URL + "/" + process.env.REACT_APP_API_PROJECT_KEY + "/products/" + id, { headers: {"Authorization" : `Bearer ${localStorage.getItem('ecomm_token')}`}
+}).
+  then(response => { setProduct(response.data) }).
+  catch(error => { console.error(error) }); 
+},[]); 
+// const {masterData:{current:{name:{en}}}} =  product;  
+  //  console.log(product?.masterData?.current?.name?.en);
+let itemName = product?.masterData?.current?.name?.en;
   return (
-    <div className='product-detail'>        
+    <div className='product-detail'> 
+       {itemName}
     </div>
   )
 }
