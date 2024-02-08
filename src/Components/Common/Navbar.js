cf347b32fa921ee8c14e2e7885408550395237be
Inderjeet Singh (PHP) <inderjeet.singh.1@nsi.lab>
2024-02-08 16:17:48 +0530

    Menu Updated

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -23,10 +23,10 @@ function Navbar(props) {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <NavLink to="/"><li className="nav-item">Home</li></NavLink>
-              <NavLink to="#"><li className="nav-item">Shop</li></NavLink>
-              <NavLink to="#"><li className="nav-item">Offers</li></NavLink>
-              <NavLink to="contact"><li className="nav-item">Contact us</li></NavLink>
+              <li className="nav-item"><NavLink to="/">Home</NavLink></li>
+              <li className="nav-item"><NavLink to="shop">Shop</NavLink></li>
+              <li className="nav-item"><NavLink to="offers">Offers</NavLink></li>
+              <li className="nav-item"><NavLink to="contact">Contact us</NavLink></li>
                 {isLogged ? (<li className="nav-item">
                       <Link to="login"><span>Log in</span></Link>
                 </li>):(<li className="nav-item">
