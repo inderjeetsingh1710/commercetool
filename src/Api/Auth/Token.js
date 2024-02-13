106685a8783563d78ec6683bb038aea4ae452982
Ashwinder <ashwinder.sharma@netsolutions.com>
2024-02-13 12:51:03 +0530

    fixed token

--- src/Api/Auth/Token.js
+++ src/Api/Auth/Token.js
@@ -12,45 +12,46 @@ function getDate() {
 function Token() {
   // const history = useNavigate();
   const [login, setLogin] = useState([]);
-  const [userName, createUser] = useState(''); 
+  const [userName, createUser] = useState('');
   const postData = {
-        username: 'xFAvcurovwyUtd5l-0OqKLVO',
-        password: 'Nh0xgihKwYbqayKHcRfYlgTU-aUy29My',
-        // expiresInMins: 60, // optional
-    }
-    const headres = {
-      'Authorization': 'Basic eEZBdmN1cm92d3lVdGQ1bC0wT3FLTFZPOk5oMHhnaWhLd1licWF5S0hjUmZZbGdUVS1hVXkyOU15'      
-    };
-    try {
-      let token = localStorage.getItem('ecomm_token');
-      let get_token_date = localStorage.getItem('get_ecomm_token_date');
-      let todayDate = getDate();
-      console.log('API token: '+token);      
-      console.log('API today: '+get_token_date);  
-      
-      const fetchToken = async () => {      
-        if(get_token_date != todayDate || token != ''){   
-          console.log('API Call for Token');     
-          await axios.request({
-            url: 'https://auth.eu-central-1.aws.commercetools.com/oauth/token?grant_type=client_credentials',
-            method:'post',
-            headers: headres,
-            data: postData
-          }).then(function(response){
-              let responseData = JSON.parse(JSON.stringify(response)); 
-              console.log('responseData1: '+responseData.data.access_token);  
-              localStorage.setItem('ecomm_token', responseData.data.access_token);
-              localStorage.setItem('get_ecomm_token_date', getDate());  
-              //let token = localStorage.getItem('token'); 
-          });
-        }    
-      };
-      fetchToken(); 
+    username: 'xFAvcurovwyUtd5l-0OqKLVO',
+    password: 'Nh0xgihKwYbqayKHcRfYlgTU-aUy29My',
+    // expiresInMins: 60, // optional
+  }
+  const headres = {
+    'Authorization': 'Basic eEZBdmN1cm92d3lVdGQ1bC0wT3FLTFZPOk5oMHhnaWhLd1licWF5S0hjUmZZbGdUVS1hVXkyOU15'
+  };
+  try {
+    let token = localStorage.getItem('ecomm_token');
+    let get_token_date = localStorage.getItem('get_ecomm_token_date');
+    let todayDate = getDate();
 
-    }
-    catch (e) {
-      console.log(e)
-    }  
-     
+
+    const fetchToken = async () => {
+
+      if (get_token_date != todayDate || token == '') {
+
+        localStorage.setItem('ecomm_token', '');
+        await axios.request({
+          url: 'https://auth.eu-central-1.aws.commercetools.com/oauth/token?grant_type=client_credentials',
+          method: 'post',
+          headers: headres,
+          data: postData
+        }).then(function (response) {
+          let responseData = JSON.parse(JSON.stringify(response));
+          localStorage.setItem('ecomm_token', responseData.data.access_token);
+          localStorage.setItem('get_ecomm_token_date', getDate());
+          //let token = localStorage.getItem('token'); 
+        });
+      }
+
+    };
+    fetchToken();
+
+  }
+  catch (e) {
+    console.log(e)
+  }
+
 }
 export default Token
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,43 +6,43 @@ import React, { useEffect, useState, useContext } from "react";
 
 
 
-function MyBanner(){
+function MyBanner() {
   return (
-    <div className="banner">        
-        <div className="container">
-          <div className="quick-info">
-              <ul>
-                <li className="col-md-3 col-sm-3">
-                  <img width="35" height="35" src="/delivery-icon.png" alt="" title="" />
-                  <a href="#">What is Lorem Ipsum?</a>
-                </li>
-                <li className="col-md-3 col-sm-3">
-                  <img width="35" height="35" src="/fifm_green.png" alt="" title="" />
-                  <a href="#">Why do we use it?</a>
-                </li>
-                <li className="col-md-3 col-sm-3">
-                  <img width="35" height="35" src="/clock-icon.png" alt="" title="" />
-                  <a href="#">Where does it come from?</a>
-                </li>
-                <li className="col-md-3 col-sm-3">
-                  <img width="35" height="35" src="/stock-icon.png" alt="" title="" />
-                  <a href="#">Where can I get some?</a>
-                </li>
-              </ul>
+    <div className="banner">
+      <div className="container">
+        <div className="quick-info">
+          <ul>
+            <li className="col-md-3 col-sm-3">
+              <img width="35" height="35" src="/delivery-icon.png" alt="" title="" />
+              <a href="#">What is Lorem Ipsum?</a>
+            </li>
+            <li className="col-md-3 col-sm-3">
+              <img width="35" height="35" src="/fifm_green.png" alt="" title="" />
+              <a href="#">Why do we use it?</a>
+            </li>
+            <li className="col-md-3 col-sm-3">
+              <img width="35" height="35" src="/clock-icon.png" alt="" title="" />
+              <a href="#">Where does it come from?</a>
+            </li>
+            <li className="col-md-3 col-sm-3">
+              <img width="35" height="35" src="/stock-icon.png" alt="" title="" />
+              <a href="#">Where can I get some?</a>
+            </li>
+          </ul>
+        </div>
+        <div className="row home-banners">
+          <div className="col-md-12">
+            <img src="/banner-1.png" />
           </div>
-          <div className="row home-banners">
-              <div className="col-md-12">
-                  <img src="/banner-1.png" />
-              </div>              
-          </div>
-        </div>                
+        </div>
+      </div>
     </div>
-    
+
   );
 }
-const sleep = ms =>  new Promise(resolve => setTimeout(resolve, ms));
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-function FeaturedProduct(){
+function FeaturedProduct() {
   const { cartItems, addToCart } = useContext(CartContext)
   const [myProductList, setPList] = useState([]);
   const [myHomeProducts, setHomeProductList] = useState([]);
@@ -50,66 +50,64 @@ function FeaturedProduct(){
   const currencySymbol = process.env.REACT_APP_CURRENCY_SYMBOL;
 
   const fetchProducts = async () => {
-    let token = localStorage.getItem('ecomm_token');   
+    let token = localStorage.getItem('ecomm_token');
+    console.log('fetchproducts_home_page', token);
     let paramId = 'c7cc7bb6-5de4-4100-8b78-a0f69776dbb3';
     const headers = {
-    'Authorization': 'Bearer '+token,
-    'Content-Type': 'application/json'      
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
     };
-    const apiUrl = process.env.REACT_APP_ECOMM_API_URL+'/'+process.env.REACT_APP_ECOMM_PROJ_NAME+'/products/?limit=4&where=masterData(current(categories(id%3D%22'+paramId+'%22)))';
+    const apiUrl = process.env.REACT_APP_ECOMM_API_URL + '/' + process.env.REACT_APP_ECOMM_PROJ_NAME + '/products/?limit=4&where=masterData(current(categories(id%3D%22' + paramId + '%22)))';
     await axios.request({
-        url: apiUrl,
-        method:'get',
-        headers: headers      
-    }).then(function(response){
-        let categories = JSON.parse(JSON.stringify(response));            
-        //console.log('Categories: '+categories.data.results[0].id);
-        //return false;
-        let tempProduct = {          
-        }       
-        setPList(categories.data.results);            
+      url: apiUrl,
+      method: 'get',
+      headers: headers
+    }).then(function (response) {
+      let categories = JSON.parse(JSON.stringify(response));
+      let tempProduct = {
+      }
+      setPList(categories.data.results);
     });
 
   };
 
   useEffect(() => {
     let token = localStorage.getItem('ecomm_token');
-    console.log('FeaturedProduct: '+token);
-    if(token == '' || token == null ){
+    if (token == '' || token == null) {
       sleep(2000).then(() => {
-        console.log('Going into Sleep 2');
+
         fetchProducts();
       });
-    }else{
+    } else {
       fetchProducts();
-    } 
-    
-  }, []);
-  return(
-    <div className="product-list">
-    <div className="container">
-      <h2><span>Featured Products</span></h2>
-      <hr className="style-div" />      
-      <div className="row">
-      { (myProductList.length > 0) ? myProductList.map((prolist) => <div className="col-md-3" key={prolist.id}>                
-                <div className="inner-product"> 
-                    <div className="pro-img">
-                    <Link to={`/product/${prolist.id}`}><img src={prolist.masterData.current.variants[0].images[0].url} alt={prolist.masterData.current.name.en} /></Link>
-                    </div>                  
-                    <div className="inner-cat-info">                           
-                      <Link to={`/product/${prolist.id}`}><h1>{prolist.masterData.current.name.en}</h1></Link>
-                          {/* { list.masterData.current.masterVariant.prices.map(prices => <p>{prices.value.centAmount}</p>) }                            */}
-                          <div className="price-container">{(prolist.masterData.current.masterVariant.prices[0].value.centAmount / 100).toLocaleString("en-US", {style:"currency", currency:currency})}</div>                          
-                          <div className="btn-container"><a href="#" className="addToCart cart-btn btn btn-success" data-id={prolist.id} onClick={() => addToCart({'id':prolist.id,'title':prolist.masterData.current.name.en,'price': (prolist.masterData.current.masterVariant.prices[0].value.centAmount / 100).toFixed(2) ,'thumbnail':prolist.masterData.current.variants[0].images[0].url})}><i className="bi bi-bag"></i> Add to Cart</a></div>
-                    </div>
-                </div>
-            
-            </div>
-          ) : <div className='col-md-12'><p>No Product Found</p></div> }
-      </div>
-    </div>
+    }
 
-  </div>
+  }, []);
+  return (
+    <div className="product-list">
+      <div className="container">
+        <h2><span>Featured Products</span></h2>
+        <hr className="style-div" />
+        <div className="row">
+          {(myProductList.length > 0) ? myProductList.map((prolist) => <div className="col-md-3" key={prolist.id}>
+            <div className="inner-product">
+              <div className="pro-img">
+                <Link to={`/product/${prolist.id}`}><img src={prolist.masterData.current.variants[0].images[0].url} alt={prolist.masterData.current.name.en} /></Link>
+              </div>
+              <div className="inner-cat-info">
+                <Link to={`/product/${prolist.id}`}><h1>{prolist.masterData.current.name.en}</h1></Link>
+                {/* { list.masterData.current.masterVariant.prices.map(prices => <p>{prices.value.centAmount}</p>) }                            */}
+                <div className="price-container">{(prolist.masterData.current.masterVariant.prices[0].value.centAmount / 100).toLocaleString("en-US", { style: "currency", currency: currency })}</div>
+                <div className="btn-container"><a href="#" className="addToCart cart-btn btn btn-success" data-id={prolist.id} onClick={() => addToCart({ 'id': prolist.id, 'title': prolist.masterData.current.name.en, 'price': (prolist.masterData.current.masterVariant.prices[0].value.centAmount / 100).toFixed(2), 'thumbnail': prolist.masterData.current.variants[0].images[0].url })}><i className="bi bi-bag"></i> Add to Cart</a></div>
+              </div>
+            </div>
+
+          </div>
+          ) : <div className='col-md-12'><p>No Product Found</p></div>}
+        </div>
+      </div>
+
+    </div>
   );
 }
 function Home() {
@@ -123,16 +121,16 @@ function Home() {
   // Products
   const [mylist, setList] = useState([]);
   const [myCatlist, setList1] = useState([]);
- 
-  useEffect(() => {    
+
+  useEffect(() => {
     let token = localStorage.getItem('ecomm_token');
-    console.log('fetchCategories token: '+token);
-    if(token == '' || token == null){
+    console.log('fetchCategories token: ' + token);
+    if (token == '' || token == null) {
       sleep(2000).then(() => {
-        console.log('Going into Sleep 1');
+
         fetchCategories();
       });
-    }else{
+    } else {
       fetchCategories();
     }
   }, []);
@@ -141,45 +139,42 @@ function Home() {
   // https://api.eu-central-1.aws.commercetools.com/netsolutionssample/categories
 
   const fetchCategories = async () => {
-    let token = localStorage.getItem('ecomm_token');     
+    let token = localStorage.getItem('ecomm_token');
     const headers = {
-      'Authorization': 'Bearer '+token,
-      'Content-Type': 'application/json'      
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/json'
     };
-    const apiUrl = process.env.REACT_APP_ECOMM_API_URL+'/'+process.env.REACT_APP_ECOMM_PROJ_NAME+'/categories?limit=4';
+    const apiUrl = process.env.REACT_APP_ECOMM_API_URL + '/' + process.env.REACT_APP_ECOMM_PROJ_NAME + '/categories?limit=4';
     await axios.request({
       url: apiUrl,
-      method:'get',
-      headers: headers      
-    }).then(function(response){
-        let categories = JSON.parse(JSON.stringify(response));
-        setList1(categories.data.results); 
-        console.log('Categories: '+categories.data.results);  
-        
+      method: 'get',
+      headers: headers
+    }).then(function (response) {
+      let categories = JSON.parse(JSON.stringify(response));
+      setList1(categories.data.results);
+
+
     });
 
-    //const response = await axios.get("https://api.eu-central-1.aws.commercetools.com/netsolutionssample/categories");
-    //console.log(response.data);
-    //setList(response.data);
   };
- 
+
   return (
     <><MyBanner />
       <div className="categories-list">
         <div className="container">
           <h2><span>Shop by Category</span></h2>
           <hr className="style-div" />
-          <div className="row latest-product-offers home-categories" id="home-categories">          
-            
-            {myCatlist.map((catlist,index) => <div className="col-md-3" key={catlist.id}>
+          <div className="row latest-product-offers home-categories" id="home-categories">
+
+            {myCatlist.map((catlist, index) => <div className="col-md-3" key={catlist.id}>
               <Link to={`/category/${catlist.id}`}>
                 <div className="inner-product">
-                  <div className="pro-img" style={{ backgroundImage:`url(${'/images/'+catImages[index]})`, backgroundSize: 'cover',backgroundRepeat:'no-repeat'}} >
+                  <div className="pro-img" style={{ backgroundImage: `url(${'/images/' + catImages[index]})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} >
                     {/* <img src={'/images/'+catImages[index]} alt={index} />                     */}
                   </div>
                   <div className="inner-cat-info">
                     <h1>{catlist.name.en}</h1>
-                    <p>{catlist.description ? (catlist.description.en ): ('')}</p>
+                    <p>{catlist.description ? (catlist.description.en) : ('')}</p>
                   </div>
                 </div>
               </Link>
@@ -188,7 +183,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <FeaturedProduct />   
+      <FeaturedProduct />
     </>
   );
 }
